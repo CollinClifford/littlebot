19e60d8cd977077e7598f6cfb42a2aeceb024023
CollinClifford <collinclifford@ymail.com>
2022-10-20 20:32:38 -0700

    uncommented, whoops

--- main.py
+++ main.py
@@ -20,16 +20,16 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    # if message.content.lower().startswith(
-    #         'ho') or message.content.lower().find('ho ho') != -1:
-    #     await message.channel.send('Ho ho!')
+    if message.content.lower().startswith(
+            'ho') or message.content.lower().find('ho ho') != -1:
+        await message.channel.send('Ho ho!')
 
-    # if message.content.lower().find('bye') != -1:
-    #     await message.channel.send(f'{message.author} is irretrievably lost')
+    if message.content.lower().find('bye') != -1:
+        await message.channel.send(f'{message.author} is irretrievably lost')
 
-    # if message.content.lower().find("thirsty") != -1 or message.content.lower(
-    # ).find("hungry") != -1 or message.content.lower().find("sad") != -1:
-    #     await message.channel.send('Put a tiger in your tank!')
+    if message.content.lower().find("thirsty") != -1 or message.content.lower(
+    ).find("hungry") != -1 or message.content.lower().find("sad") != -1:
+        await message.channel.send('Put a tiger in your tank!')
 
     if message.content.startswith("$quote"):
         await message.channel.send(random.choice(quotes))
