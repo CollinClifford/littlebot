29863a5698a11a41a3fce6b5cada35987311f580
CollinClifford <collinclifford@ymail.com>
2022-10-25 11:02:57 -0700

    WHY GOD?!

--- bot/message_handler.py
+++ bot/message_handler.py
@@ -39,8 +39,7 @@ async def handle_message(message):
     #     await quotes.append(message.content)
 
     if message.content.startswith("$quote Collin"):
-        await message.channel.send("hi")
-        # await message.channel.send(quotes2[random.choice('Collin')])
+        await message.channel.send(quotes2[random.choice('Collin')])
     elif message.content == "$quote" and message.content.contains("1Q84"):
         await message.channel.send(quotes2[random.choice('1Q84')])
     elif message.content == "$quote" and message.content.contains("The Glass Bees"):
