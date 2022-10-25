ab1528ea76dd7c958e1cdf3ff2c77e3a6a707031
CollinClifford <collinclifford@ymail.com>
2022-10-24 18:11:47 -0700

    trying but tired

--- bot/message_handler.py
+++ bot/message_handler.py
@@ -5,9 +5,9 @@ from bot.quotes import quotes
 from setup import client
 import json
 
-# f = open('quotes2.json')
-# data = json.load(f)
-# print(data)
+f = open('quotes2.json')
+data = json.load(f)
+print(data)
 
 async def handle_message(message):
     if message.author == client.user:
