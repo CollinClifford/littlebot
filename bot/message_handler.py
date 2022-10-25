fcf79e75b640fddfd93572d02d3ac975ab29d3b5
CollinClifford <collinclifford@ymail.com>
2022-10-24 18:14:46 -0700

    trying but tired

--- bot/message_handler.py
+++ bot/message_handler.py
@@ -16,7 +16,8 @@ async def handle_message(message):
     # --- COMMANDS --- #
 
     if message.content == "$quote":
-        await message.channel.send(random.choice(quotes)) and print(data)
+        await message.channel.send(random.choice(quotes))
+        await print(data)
     elif message.content == "$books":
         for indx, book in enumerate(total_books):
             await message.channel.send(f'{indx + 1}) {book}')
