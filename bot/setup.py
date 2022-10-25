0ff21cd6311196f1ecafc9329d5e499bd76e7c00
CollinClifford <collinclifford@ymail.com>
2022-10-25 10:52:37 -0700

    commented out keys

--- bot/setup.py
+++ bot/setup.py
@@ -3,8 +3,8 @@ from decouple import config
 from goodreads import client
 
 discord_token = config('TOKEN')
-goodreads_api_key = config("GOODREADS_API_KEY")
-goodreads_api_secret = config("GOODREADS_API_SECRET")
+# goodreads_api_key = config("GOODREADS_API_KEY")
+# goodreads_api_secret = config("GOODREADS_API_SECRET")
 
 # ----- discord ----- #
 intents = discord.Intents.default()
@@ -13,4 +13,4 @@ intents.message_content = True
 discord_client = discord.Client(intents=intents)
 
 # ----- goodreads ----- #
-goodreads_client = client.GoodreadsClient(goodreads_api_key, goodreads_api_secret)
+# goodreads_client = client.GoodreadsClient(goodreads_api_key, goodreads_api_secret)
